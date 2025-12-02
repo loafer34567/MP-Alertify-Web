ca31127b432aa87ef252b0c1d11cbc49d2230f8d
mikla1122 <143484797+mikla1122@users.noreply.github.com>
2025-12-02 20:44:56 +0800

    change port

--- app.py
+++ app.py
@@ -224,5 +224,5 @@ def disable_user():
 # Run server
 # ---------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5500))
     app.run(host="0.0.0.0", port=port)
